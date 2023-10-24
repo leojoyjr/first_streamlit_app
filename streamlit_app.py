32ae9b090d20c09ada388fec3cb898d38afbd676
Leo Joy <52007885+leojoyjr@users.noreply.github.com>
2023-10-24 19:31:47 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omlete')
+streamlit.text('Hard boiled eggs')
+streamlit.text('Spinach Curry')
